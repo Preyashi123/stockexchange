6feee24834ce04813d5ff096309661de609f4746
Amlan66 <36122995+Amlan66@users.noreply.github.com>
2020-08-31 21:55:37 +0530

    Update UserService.java

--- com/wellsfargo/stockmarket/loginsignup/service/UserService.java
+++ com/wellsfargo/stockmarket/loginsignup/service/UserService.java
@@ -19,26 +19,13 @@ public class UserService {
 	UserRepository userRepository;
 	User user;
 	
-	//userRepository.findUser(username, password).equals(username)
-	
-//	public boolean findinTable(String username, String password) {
-//		
-//			if(userRepository.findUser(username) != null) {
-//				if(userRepository.findUser(username).equals(password))
-//					return true;
-//				else
-//					return false;
-//			}
-//				
-//			else
-//				return false;
-//	}
+
 	public String findbyID(String username,String password) {
 		
-		//Optional<User> userEn =  userRepository.findById(username);
+		
 		user = userRepository.findByUserName(username);
 		if(user != null) {
-			//User userEntity = userEn.get();
+			
 			if(user.getPassword().equals(password))
 				return "<h1>SUCCESS</h1>";
 			else
@@ -78,57 +65,5 @@ public class UserService {
 		else 
 			return "<h1> USER NOT PRESENT</h1>";
 	}
-
-	/*public String updateUser(User user) {
-		 
-		// TODO Auto-generated method stub
-		String username=user.getUserName();
-		Optional<User> userEn = userRepository.findById(username);	
-		User u = userEn.get();
-		if(u.getUserName().equals(username)) {
-			userRepository.save(user);
-			return "<h1> USER_UPDATED</h1>";
-		}
-		else 
-			return "<h1> USER NOT UPDATED</h1>";
-	
-	}*/
-
-	/*public void addUser(boolean isAdmin,String username,String password, String email,boolean verified) {
-		User user=new User(isAdmin,username,password,email,verified);
-		Optional<User> userEn =  userRepository.findById(username);
-		if(userEn == null){
-			userRepository.save(user);
-		else {
-			
-		}
-		if(userEn != null) {
-		    return true;
-		}
-		else
-			return false;
-		}
-		
-	}
-	}
-	*/
-	
-	
-	// all the functions
-	//get
-	//update
-	//new
-	
-	
-//	public User checkUsername (String username){
-//		User xyz =  userRepository.findByUsernameContaining(username);
-//		System.out.println(xyz.toString());
-//		return xyz;
-//	}
-//	
-//	@PostMapping("/{userId}")
-//	public User createUser(@RequestBody User user) {
-//		return this.userRepository.save(User);
-//	}
 
 }
